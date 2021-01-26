e4eb4a96bbf58a90844503ebe25492a2977432c3
Barry Mattern <bdmattern@gmail.com>
2021-01-26 16:14:07 -0700

    Work on reviews/reservations routes

---
+++ python-project-starter-main/react-app/src/components/AddReview/index.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import createReview from '../../store/reviews';
+
+// const AddReview = () => {
+//     const reviews = useSelector(state => {
+//         console.log(state);
+//         return state;
+//     });
+
+//     return (
+//         <h1>These are reviews</h1>
+//     );
+// };
+
+// export default AddReview;
