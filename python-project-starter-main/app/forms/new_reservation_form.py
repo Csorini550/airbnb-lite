4848af70639d682ea1519477e956eda1c8c6a3ae
Angelica <angelica.wilson93@gmail.com>
2021-01-27 13:03:52 -0600

    Reservations post route functional

--- python-project-starter-main/app/forms/new_reservation_form.py
+++ python-project-starter-main/app/forms/new_reservation_form.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, FloatField, TextAreaField, DateTimeField
+from wtforms import StringField, IntegerField, FloatField, TextAreaField, DateTimeField, DateField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import Review
 
 class NewReservationForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
     venue_id = IntegerField('venue_id', validators=[DataRequired()])
-    start_date = DateTimeField('start_date', validators=[DataRequired()], format="%Y-%m-%dT%H:%M")
-    end_date = DateTimeField('end_date', validators=[DataRequired()], format="%Y-%m-%dT%H:%M")
+    start_date = DateTimeField('start_date', validators=[DataRequired()])
+    end_date = DateTimeField('end_date', validators=[DataRequired()])
     price = FloatField('price', validators=[DataRequired()])
     total = FloatField('total', validators=[DataRequired()])
     guest_count = IntegerField('guest_count')
