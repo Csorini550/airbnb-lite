4438f61d74937e3872680a702b73c7786076d115
Christopher Sorini <csorini13@gmail.com>
2021-01-27 10:55:18 -0600

    Trying again

---
+++ python-project-starter-main/react-app/src/store/venue.js
@@ -0,0 +1,15 @@
+// import { fetch } from './csrf';
+// const initialState = {}
+
+// const CREATE_VENUE = ''
+
+// const createVenueAction = (venue) => {
+//     type: CREATE_VENUE,
+//         payload: venue
+// }
+// export const createVenue = (venue) => {
+//     return async (dispatch) => {
+//         const res = await fetch(`/api/venues`)
+//         dispatch(createVenueAction)
+//     }
+// }
