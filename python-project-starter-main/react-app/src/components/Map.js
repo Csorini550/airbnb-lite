1c385c1c8aa0ecfc6de10292567042291d83855b
Elyse Steingold <elyselindsays@gmail.com>
2021-03-04 16:02:30 -0500

    working

--- python-project-starter-main/react-app/src/components/Map.js
+++ python-project-starter-main/react-app/src/components/Map.js
@@ -9,10 +9,10 @@ const Map = ({ address, city, state }) => {
   let geocoded;
   let oneLineAddress = `${address} ${city} ${state}`;
 
-  const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyCJMuSmneTfi_MCV_-PK-DpQSOA7-aw4uU" // ,
-    // ...otherOptions
-  })
+  // const { isLoaded, loadError } = useJsApiLoader({
+  //   googleMapsApiKey: "AIzaSyCJMuSmneTfi_MCV_-PK-DpQSOA7-aw4uU" // ,
+  //   // ...otherOptions
+  // })
 
 
   const mapContainerStyle = {
@@ -20,43 +20,43 @@ const Map = ({ address, city, state }) => {
     height: "200px",
   }
 
-  const renderMap = () => {
-    // wrapping to a function is useful in case you want to access `window.google`
-    // to eg. setup options or create latLng object, it won't be available otherwise
-    // feel free to render directly if you don't need that
-    const onLoad = useCallback(
-      function onLoad(mapInstance) {
-        const geocoder = new google.maps.Geocoder();
-        geocoder.geocode({ 'address': oneLineAddress }, function (results, status) {
-          if (status == 'OK') {
-            geocoded = results[0].geometry.location;
+  // const renderMap = () => {
+  //   // wrapping to a function is useful in case you want to access `window.google`
+  //   // to eg. setup options or create latLng object, it won't be available otherwise
+  //   // feel free to render directly if you don't need that
+  //   const onLoad = useCallback(
+  //     function onLoad(mapInstance) {
+  //       const geocoder = new google.maps.Geocoder();
+  //       geocoder.geocode({ 'address': oneLineAddress }, function (results, status) {
+  //         if (status == 'OK') {
+  //           geocoded = results[0].geometry.location;
 
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        })
-      }
-    )
-    return <GoogleMap
-      // options={options}
-      onLoad={onLoad}
-      mapContainerStyle={mapContainerStyle}
-      zoom={13}
-      center={geocoded}
-    >
+  //         } else {
+  //           alert('Geocode was not successful for the following reason: ' + status);
+  //         }
+  //       })
+  //     }
+  //   )
+  return <GoogleMap
+  // options={options}
+  // onLoad={onLoad}
+  // mapContainerStyle={mapContainerStyle}
+  // zoom={13}
+  // center={geocoded}
+  >
 
-      <Marker position={geocoded} />
+    {/* <Marker position={geocoded} /> */}
 
-    </GoogleMap>
-  }
-
-
-  if (loadError) {
-    return <div>Map cannot be loaded right now, sorry.</div>
-  }
-
-  return isLoaded ? renderMap() : null
+  </GoogleMap>
 }
+
+
+// if (loadError) {
+//   return <div>Map cannot be loaded right now, sorry.</div>
+// }
+
+// return isLoaded ? renderMap() : null
+// }
 
 
 
