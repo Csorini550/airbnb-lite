8dd1321eef85cdd91243ffb3bed3507e640e9bf6
Christopher Sorini <csorini13@gmail.com>
2021-03-04 12:44:26 -0600

    Date check is working with total price, bug where if goes into a new month the price is not correct

--- python-project-starter-main/react-app/src/components/Reservations/index.js
+++ python-project-starter-main/react-app/src/components/Reservations/index.js
@@ -14,7 +14,7 @@ const Reservations = () => {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [price, setPrice] = useState("");
-  const [total, setTotal] = useState("");
+
   const [guestCount, setGuestCount] = useState("");
   let { venueId } = useParams();
 
@@ -50,6 +50,7 @@ const Reservations = () => {
   const venue = useSelector((state) => {
     return state.search[venueId]
   });
+  const [total, setTotal] = useState(venue.price);
 
   useEffect(() => {
     dispatch(getVenue(venue.id));
@@ -71,7 +72,7 @@ const Reservations = () => {
         start_date: startDate,
         end_date: endDate,
         price: venue.price,
-        total: venue.price,
+        total: totalPrice,
         guest_count: 1
       })
     });
@@ -80,9 +81,18 @@ const Reservations = () => {
     history.push(`/create-review/${venueId}`);
   }
 
-  // let totalPrice = () => {
-  //     let total = price * days;
-  // };
+  let totalPrice = (total) => {
+    console.log("total", total)
+    console.log("start date", startDate)
+    console.log("end date", endDate)
+    let end = endDate.split("-")
+    let finalDate = parseInt(end[2])
+    let start = startDate.split("-")
+    let beginDate = parseInt(start[2])
+    let endPrice = venue.price * (finalDate - beginDate);
+    console.log("endPrice", endPrice)
+    setTotal(endPrice)
+  };
 
   // Prevents "undefined" error from trying to load before state is updated
   if (Object.keys(venue).length === 0) return null;
@@ -147,10 +157,10 @@ const Reservations = () => {
                   </label>
                 </div>
                 <div id="reserve-btn">
-                  <button id="availability">Check Availability</button>
+                  <button onClick={totalPrice} id="availability">Check Total Price</button>
                 </div>
                 <div>
-                  <h3>Total Price: {venue.price}</h3>
+                  <h3>Total Price: ${total}</h3>
                 </div>
                 <div id="reserve-btn">
                   <button id="reserve">Reserve</button>
