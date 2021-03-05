eb93d4e97b60b8ed845da17b148e840ee3cb656b
Angelica <angelica.wilson93@gmail.com>
2021-03-05 17:54:52 -0600

    Add my footer info

--- python-project-starter-main/react-app/src/components/Footer/index.js
+++ python-project-starter-main/react-app/src/components/Footer/index.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
 import "./Footer.css"
 import chrisPic from "./profilePic1.jpg"
+import angelicaPic from "./IMG_0883.JPG"
 const Footer = () => {
 
 
@@ -19,12 +20,12 @@ const Footer = () => {
                 <h1>Chris Sorini</h1>
             </div>
             <div className="footer-container">
-                <a href="https://github.com/Csorini550"> <AiFillGithub className="icon" /> </a>
-                <a href="http://chrissorini.com/">
-                    <img title="Portfoli Website" className="image-wrap" src={chrisPic} id="personal-pic"></img>
+                <a href="https://github.com/anwilson93"> <AiFillGithub className="icon" /> </a>
+                <a href="http://angelicawilson.com/">
+                    <img title="Portfoli Website" className="image-wrap" src={angelicaPic} id="personal-pic"></img>
                 </a>
                 <p class="img-description">Portfoli website</p>
-                <a href="https://www.linkedin.com/in/christopher-t-41a491206/"> <AiFillLinkedin className="icon" /> </a>
+                <a href="https://www.linkedin.com/in/angelica-wilson-579b0b202/"> <AiFillLinkedin className="icon" /> </a>
                 <h1>Angelica Wilson</h1>
             </div>
             <div className="footer-container">
