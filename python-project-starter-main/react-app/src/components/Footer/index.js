4ed9bf025819ef5649fea152bac9dd9013a0fb5d
Elyse Steingold <elyselindsays@gmail.com>
2021-03-11 23:05:27 -0500

    added footer info

--- python-project-starter-main/react-app/src/components/Footer/index.js
+++ python-project-starter-main/react-app/src/components/Footer/index.js
@@ -5,6 +5,7 @@ import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
 import "./Footer.css"
 import chrisPic from "./profilePic1.jpg"
 import angelicaPic from "./IMG_0883.JPG"
+import elysePic from "./profileimg.jpg"
 const Footer = () => {
 
 
@@ -29,12 +30,12 @@ const Footer = () => {
                 <h1>Angelica Wilson</h1>
             </div>
             <div className="footer-container">
-                <a href="https://github.com/Csorini550"> <AiFillGithub className="icon" /> </a>
-                <a href="http://chrissorini.com/">
-                    <img title="Portfoli Website" src={chrisPic} id="personal-pic"></img>
+                <a href="https://github.com/elyselindsays"> <AiFillGithub className="icon" /> </a>
+                <a href="http://elysecodes.elysesteingold.com/">
+                    <img title="Portfoli Website" src={elysePic} id="personal-pic"></img>
                 </a>
                 <p className="img-description">Portfoli website</p>
-                <a href="https://www.linkedin.com/in/christopher-t-41a491206/"> <AiFillLinkedin className="icon" /> </a>
+                <a href="https://www.linkedin.com/in/elyse-steingold-2234b299/"> <AiFillLinkedin className="icon" /> </a>
                 <h1>Elyse Steingold</h1>
             </div>
         </div >
