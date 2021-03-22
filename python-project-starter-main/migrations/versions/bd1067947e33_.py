63ffe90f9c5aac1c11d077c93987f8f91228ee23
Christopher Sorini <csorini13@gmail.com>
2021-03-22 13:43:15 -0500

    mostly working now

--- python-project-starter-main/migrations/versions/bd1067947e33_.py
+++ python-project-starter-main/migrations/versions/bd1067947e33_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'bd1067947e33'
-down_revision = '6760d2cb1afa'
+down_revision = None #'6760d2cb1afa'
 branch_labels = None
 depends_on = None
 
